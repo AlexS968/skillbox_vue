679b6e47db2697c4b90566eff27deac8f0a9a74f
AlexS968 <61356947+AlexS968@users.noreply.github.com>
2021-02-11 18:10:23 +0100

    added components ProductList and ProductItem

---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: 'Универсальный триммер Philips MG3731/15',
+    price: 3390,
+    image: 'img/trimmer.jpg',
+  },
+  {
+    title: 'Электрическая зубная щетка Philips Sonicare HX6232/41',
+    price: 6990,
+    image: 'img/toothbrush.jpg',
+  },
+  {
+    title: 'Перфоратор Bosch GBH 2-28 F New',
+    price: 14858,
+    image: 'img/hammerdrill.jpg',
+  },
+  {
+    title: 'Бритва для бритья GIllette Fusion5 Proglide Power 11045',
+    price: 1020,
+    image: 'img/gilette.jpg',
+  },
+  {
+    title: 'Умные часы Samsung Galaxy Watch Active 2',
+    price: 17451,
+    image: 'img/watch.jpg',
+  },
+  {
+    title: 'Наушники беспроводные TWS iNeez TW-16 с LED дисплеем',
+    price: 995,
+    image: 'img/headphones.jpg',
+  },
+  {
+    title: 'Батарейки DURACELL AA (LR6), экономичная упаковка, 12 шт',
+    price: 399,
+    image: 'img/batteries.jpg',
+  },
+  {
+    title: 'Чернила для принтера Revcol Canon-GI-490-4цвета',
+    price: 896,
+    image: 'img/printerink.jpg',
+  },
+  {
+    title: 'Автомобильное зарядное устройство QC3.0 2xUSB в прикуриватель',
+    price: 399,
+    image: 'img/charger.jpg',
+  },
+  {
+    title: 'Смартфон Honor 7A 2/16GB, золотой',
+    price: 5180,
+    image: 'img/smartphone.jpg',
+  },
+  {
+    title: 'Электронная книга Amazon Kindle Oasis 2020 3 поколение 7", серый металлик',
+    price: 27095,
+    image: 'img/ebook.jpg',
+  },
+  {
+    title: 'Компактный фотоаппарат Canon PowerShot SX540 HS, черный',
+    price: 23226,
+    image: 'img/camera.jpg',
+  },
+];
