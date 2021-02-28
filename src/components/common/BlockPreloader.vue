c98695dc49d811d8933fad4ca3abc39b69f5178e
AlexS968 <61356947+AlexS968@users.noreply.github.com>
2021-02-28 11:51:30 +0100

    7.9.7 changed bucket, changed preloader

---
+++ src/components/common/BlockPreloader.vue
@@ -0,0 +1,59 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.big {
+  width: 300px;
+  height: 300px;
+  margin: 30px auto;
+}
+.small {
+  width: 35px;
+  height: 35px;
+}
+.preloader {
+  display: block;
+  position: relative;
+  border-radius: 50%;
+  border: 3px solid transparent;
+  border-top-color: #337AB7;
+  animation: preloader-spin 2s linear infinite;
+}
+.preloader:before {
+  content: "";
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  right: 5px;
+  bottom: 5px;
+  border-radius: 50%;
+  border: 3px solid transparent;
+  border-top-color: #BFE2FF;
+  animation: preloader-spin 3s linear infinite;
+}
+.preloader:after {
+  content: "";
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  right: 5px;
+  bottom: 5px;
+  border-radius: 50%;
+  border: 3px solid transparent;
+  border-top-color: #337AB7;
+  animation: preloader-spin 1.5s linear infinite;
+}
+@keyframes preloader-spin {
+  0%   {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import products from '@/data/products';
+import axios from 'axios';
+import API_BASE_URL from '@/config';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     cartProducts: [],
+
+    userAccessKey: null,
+
+    cartProductsData: [],
+
+    dataLoading: false,
   },
   mutations: {
     addProductToCart(state, { productId, amount }) {
@@ -33,14 +40,37 @@ export default new Vuex.Store({
       state.cartProducts = state.cartProducts
         .filter((item) => item.productId !== productId);
     },
+    updateUserAccessKey(state, accessKey) {
+      state.userAccessKey = accessKey;
+    },
+    updateCartProductsData(state, items) {
+      state.cartProductsData = items;
+    },
+    syncCartProducts(state) {
+      // eslint-disable-next-line
+      state.cartProducts = state.cartProductsData.map((item) => {
+        return {
+          productId: item.product.id,
+          amount: item.quantity,
+        };
+      });
+    },
+    changeDataLoading(state, dataLoading) {
+      state.dataLoading = dataLoading;
+    },
   },
   getters: {
     cartDetailProducts(state) {
       // eslint-disable-next-line
       return state.cartProducts.map((item) => {
+        const { product } = state.cartProductsData
+          .find((p) => p.product.id === item.productId);
         return {
           ...item,
-          product: products.find((p) => p.id === item.productId),
+          product: {
+            ...product,
+            image: product.image.file.url,
+          },
         };
       });
     },
@@ -51,6 +81,30 @@ export default new Vuex.Store({
     cartTotalAmount(state, getters) {
       return getters.cartDetailProducts
         .reduce((acc, item) => item.amount + acc, 0);
+    },
+    dataLoading(state) {
+      return state.dataLoading;
+    },
+  },
+  actions: {
+    loadData(context) {
+      context.commit('changeDataLoading', true);
+      this.loadDataTimer = setTimeout(() => {
+        axios.get(`${API_BASE_URL}/api/baskets`, {
+          params: {
+            userAccessKey: context.state.userAccessKey,
+          },
+        })
+          .then((response) => {
+            if (!context.state.userAccessKey) {
+              localStorage.setItem('userAccessKey', response.data.user.accessKey);
+              context.commit('updateUserAccessKey', response.data.user.accessKey);
+            }
+            context.commit('updateCartProductsData', response.data.items);
+            context.commit('syncCartProducts');
+          })
+          .then(() => context.commit('changeDataLoading', false));
+      }, 5000);
     },
   },
 });
